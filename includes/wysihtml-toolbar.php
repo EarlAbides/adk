9a235dff7b1bce4cdb1139ed38659cd29ae02ce9
Neil Simpson <neilsimp1@gmail.com>
2015-11-11 20:57:24 -0500

    wysihtml files

---
+++ includes/wysihtml-toolbar.php
@@ -0,0 +1,46 @@
+<div id="wysihtml-toolbar" style="display:none;">
+	
+	<a data-wysihtml5-command="bold">
+		<span class="glyphicon glyphicon-bold" title="Bold" data-toggle="tooltip"></span>
+	</a>
+	<a data-wysihtml5-command="italic">
+		<span class="glyphicon glyphicon-italic" title="Italic" data-toggle="tooltip"></span>
+	</a>
+	<a data-wysihtml5-command="underline">
+		<span class="glyphicon font-bold" style="font-size:21px;text-decoration:underline;" title="Underline" data-toggle="tooltip">U</span>
+	</a>
+
+	&nbsp;
+
+	<a data-wysihtml5-command="justifyLeft">
+		<span class="glyphicon glyphicon-align-left" title="Left" data-toggle="tooltip"></span>
+	</a>
+	<a data-wysihtml5-command="justifyCenter">
+		<span class="glyphicon glyphicon-align-center" title="Center" data-toggle="tooltip"></span>
+	</a>
+	<a data-wysihtml5-command="justifyRight">
+		<span class="glyphicon glyphicon-align-right" title="Right" data-toggle="tooltip"></span>
+	</a>
+
+	&nbsp;
+
+	<a data-wysihtml5-command="insertUnorderedList">
+		<span class="glyphicon glyphicon-list" title="Bullet List" data-toggle="tooltip"></span>
+	</a>
+	<a data-wysihtml5-command="insertOrderedList">
+		<span class="glyphicon glyphicon-sort-by-order" title="Bullet List" data-toggle="tooltip"></span>
+	</a>
+
+	&nbsp;
+
+	<a class="red" data-wysihtml5-command="foreColor" data-wysihtml5-command-value="red">
+		<span title="Red" data-toggle="tooltip"></span>
+	</a>
+	<a class="green" data-wysihtml5-command="foreColor" data-wysihtml5-command-value="green">
+		<span title="Green" data-toggle="tooltip"></span>
+	</a>
+	<a class="blue" data-wysihtml5-command="foreColor" data-wysihtml5-command-value="blue">
+		<span title="Blue" data-toggle="tooltip"></span>
+	</a>
+
+</div>
