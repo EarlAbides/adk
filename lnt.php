e48c5d8bee05691eaa63a01394785c8d99c3b4be
Neil Simpson <neilsimp1@gmail.com>
2016-04-09 13:45:38 -0400

    LNT updated

--- lnt.php
+++ lnt.php
@@ -29,6 +29,14 @@
 				<div class="text-center">
 					<h4><a href="https://lnt.org/" target="_blank">https://lnt.org/</a></h4>
 				</div>
+
+				<div class="hr hr75"></div>
+
+				<h4 class="content-header">One for the kids...</h4>
+				
+				<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-video" data-allowfullscreen="1" data-href="/LeaveNoTraceCenter/videos/vb.7922603659/10153582423023660/?type=3"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/LeaveNoTraceCenter/videos/10153582423023660/"><a href="https://www.facebook.com/LeaveNoTraceCenter/videos/10153582423023660/">Learning the 7 Leave No Trace Principles</a><p>Learn, remember, and teach the #LeaveNoTrace Seven Principles in a fun and interactive way!  Curious about some of the things we said in the video?  Trek through our other blogs to find out more: https://lnt.org/blog/learning-7-principles</p>Posted by <a href="https://www.facebook.com/LeaveNoTraceCenter/">Leave No Trace Center for Outdoor Ethics</a> on Wednesday, April 6, 2016</blockquote></div></div>
+								
+				<br /><br />
 				
 			</div>
 			
